40e7d72d8c59f33a2fd33dc5f7a2019bffe24034
Marlon Henry <haveyec@me.com>
2023-12-13 15:47:20 -0500

    Added xcode project, let's rock and roll

---
+++ SkillZilla_ios/SkillZilla_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SkillZilla_ios
+//
+//  Created by Marlon Henry on 12/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SkillZilla_ios/SkillZilla_ios/SkillZilla_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillZilla_iosApp.swift
+//  SkillZilla_ios
+//
+//  Created by Marlon Henry on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SkillZilla_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
